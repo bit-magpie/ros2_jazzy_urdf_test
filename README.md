98af56b3909e6fbcc496967d8f5f2fe348b8b470
isur <isuru.jayarathne@gmail.com>
2025-06-09 15:19:45 +0900

    adding all files

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
 # ROS2 Jazzy xacro URDF test
+
+Reference: https://www.youtube.com/watch?v=_F8wVuiEmww&ab_channel=AleksandarHaberPhD
+
+## Setup environment
+
+Update apt package manager
+```bash
+sudo apt update && sudo apt upgrade
+source /opt/ros/jazzy/setup.bash
+```
+
+Install dependencies
+```bash
+sudo apt install ros-jazzy-robot-state-publisher
+sudo apt install ros-jazzy-rviz2
+sudo apt install ros-jazzy-xacro
+sudo apt install ros-jazzy-ros2-control
+sudo apt install ros-jazzy-ros2-controllers
+sudo apt install ros-jazzy-gz-ros2-control
+sudo apt install ros-jazzy-gz-ros2-control-demos
+```
+
+Make the project files by cloning the repo
+```bash
+mkdir -p ~/jazzy_ws/src
+cd ~/jazzy_ws/src
+git https://github.com/bit-magpie/ros2_jazzy_urdf_test.git
+```
+
+<details>
+<summary>Make directory list if you want to create the project from the scratch</summary>
+
+```bash
+mkdir -p ~/jazzy_ws/src
+cd ~/jazzy_ws/src
+ros2 pkg create --build-type ament_cmake demo_control
+cd demo_control
+mkdir launch model config
+```
+</details>
+
+Compile and install the package
+```bash
+cd ~/jazzy_ws/
+sudo rosdep init
+rosdep update
+rosdep install -i --from-path src --rosdistro jazzy -y
+colcon build --symlink-install --packages-select demo_control
+```
+
+If the package is compiled successfully, use following commands to launch the project
+```bash
+source install/setup.bash
+ros2 launch demo_control display.launch.py
+```
+
+After Rviz is launched add `RobotModel` and set the `Description topic`to `/robot_description`.
---
+++ launch/display.launch.py
@@ -0,0 +1,134 @@
+import launch
+from launch.substitutions import Command, LaunchConfiguration
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+import launch_ros
+import os
+import xacro
+
+# define the package name
+packageName = 'demo_control'
+
+# relative path of the xacro file with respect to the package path
+xacroRelativePath = 'model/model.xacro' # 'model/example_robot.urdf.xacro' #'model/model.xacro'
+
+# RViz config file path with respect to the package path
+rvizRelativePath = 'config/config.rviz'
+
+# relative path of the ros2_control configuration file with respect to the package folder
+ros2controlRelativePath = 'config/robot_controller.yaml'
+
+def generate_launch_description():
+    # absolute package path
+    pkgPath = launch_ros.substitutions.FindPackageShare(package=packageName).find(packageName)
+    # absolute xacro model path
+    xacroModelPath = os.path.join(pkgPath, xacroRelativePath)
+    # absolute rviz config file path
+    rvizConfigPath = os.path.join(pkgPath, rvizRelativePath)
+    # absolute ros2_control config file path
+    ros2controlPath = os.path.join(pkgPath, ros2controlRelativePath)
+
+    # here, for verification, print the xacro model path
+    print(xacroModelPath)
+    # get the robot description from the xacro model file
+    robot_desc = xacro.process_file(xacroModelPath).toxml()
+    robot_description = {'robot_description': robot_desc}
+
+    # Declare arguments
+    declared_arguments = []
+    declared_arguments.append(
+        launch.actions.DeclareLaunchArgument(
+            name="gui",
+            default_value="true",
+            description="Start the RViz2 GUI."
+        )
+    )
+
+    # Initialize Arguments
+    gui = LaunchConfiguration("gui")
+
+    # for starting Gazebo
+    gazebo = launch.actions.IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [launch_ros.substitutions.FindPackageShare("ros_gz_sim"), "/launch/gz_sim.launch.py"]
+        ),
+        launch_arguments={"gz_args": "rviz3 empty.sdf"}.items(),
+        condition=launch.conditions.IfCondition(gui)
+    )
+
+    gazebo_headless = launch.actions.IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [launch_ros.substitutions.FindPackageShare("ros_gz_sim"), "/launch/gz_sim.launch.py"]
+        ),
+        launch_arguments={"gz_args": "--headless-rendering -s -rviz3 empty.sdf"}.items(),
+        condition=launch.conditions.UnlessCondition(gui)
+    )
+
+    # Gazebo bridge
+    gazebo_bridge = launch_ros.actions.Node(
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        arguments=["/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock]"],
+        output="screen"
+    )
+
+    gz_spawn_entity = launch_ros.actions.Node(
+        package="ros_gz_sim",
+        executable="create",
+        output="screen",
+        arguments=[
+            "-topic", "/robot_description",
+            "-name", "robot_system_position",
+            "-allow_renaming", "true"
+        ]
+    )
+
+    # robot state publisher node
+    robot_state_publisher_node = launch_ros.actions.Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='both',
+        parameters=[robot_description]
+    )
+
+    # rviz node
+    rviz_node = launch_ros.actions.Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        arguments=['-d', rvizConfigPath]
+    )
+
+    # ros2 control node
+    control_node = launch_ros.actions.Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[ros2controlPath],
+        output="both"
+    )
+
+    joint_state_broadcaster_spawner = launch_ros.actions.Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster"],
+    )
+
+    robot_controller_spawner = launch_ros.actions.Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["forward_position_controller", "--param-file", ros2controlPath],
+    )
+
+    nodeList = [
+        gazebo,
+        gazebo_headless,
+        gazebo_bridge,
+        gz_spawn_entity,
+        robot_state_publisher_node,
+        rviz_node,
+        control_node,
+        joint_state_broadcaster_spawner,
+        robot_controller_spawner
+    ]
+
+    return launch.LaunchDescription(declared_arguments + nodeList)
